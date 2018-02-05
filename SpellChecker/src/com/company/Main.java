5c054ffe4be11a9d2caf747e966ec908073db288
John Weissenberger <johnaweissenberger@gmail.com>
2018-02-05 18:45:16 -0500

    try

--- SpellChecker/src/com/company/Main.java
+++ SpellChecker/src/com/company/Main.java
@@ -6,6 +6,16 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
+        for (int i = 0; i < args.length; i++){
+            System.out.println(args[i] + " ")
+            if (args.length != 1){
+                System.out.println("ussage error ...");
+                System.exit(1);
+            }
+
+        }
+
+        /*
         Scanner inFile = null;
         FileInputStream myDict = null;
 
@@ -15,6 +25,6 @@ public class Main {
         String word1 = inFile.nextLine();
 
         System.out.println(word1);
-
+        */
     }
 }
