db9082a08c82f1f9b17cd2ea4a3fa76a37d6f351
John Weissenberger <johnaweissenberger@gmail.com>
2018-02-05 19:13:30 -0500

    file reader

---
+++ SpellChecker/src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            for (int i = 0; i < args.length; i++){
+                System.out.println(args[i] + " ");
+                if (args.length != 1){
+                    System.out.println("ussage error ...");
+                    System.exit(1);
+                }
+
+            }
+
+            try {
+                Scanner input = new Scanner(new File(args[0]));
+                while (input.hasNext()){
+                    String line = input.nextLine();
+                    System.out.println(line);
+                }
+
+                input.close();
+            }
+
+            catch (FileNotFoundException e){
+                System.exit(2);
+            }
+
+        /*
+        Scanner inFile = null;
+        FileInputStream myDict = null;
+
+@ -15,6 +25,6 @@ public class Main {
+        String word1 = inFile.nextLine();
+
+        System.out.println(word1);
+
+        */
+
+    }
+}
+
