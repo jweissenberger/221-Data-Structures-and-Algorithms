bf17af45a96b4cdb4b10fd512ff020a5972650b3
John Weissenberger <johnaweissenberger@gmail.com>
2018-02-06 10:08:51 -0500

    read 2 files

--- SpellChecker/src/com/company/Main.java
+++ SpellChecker/src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
             for (int i = 0; i < args.length; i++){
                 System.out.println(args[i] + " ");
-                if (args.length != 1){
+                if (args.length != 2){ //the number of files you want to read
                     System.out.println("ussage error ...");
                     System.exit(1);
                 }
@@ -17,7 +17,8 @@ public class Main {
             }
 
             try {
-                Scanner input = new Scanner(new File(args[0]));
+                Scanner input = new Scanner(new File(args[1])); //to open another file need to use args[1]
+                //and separate the arguments by a space in run-edit configurations
                 while (input.hasNext()){
                     String line = input.nextLine();
                     System.out.println(line);
