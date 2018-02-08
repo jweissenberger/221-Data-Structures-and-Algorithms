57f36885df80d9c8265aa76ac62605e9c3c22d8c
John Weissenberger <johnaweissenberger@gmail.com>
2018-02-08 14:08:36 -0500

    Fully Finish DLinkedList

--- DLinkedList/src/com/company/DLinkedList.java
+++ DLinkedList/src/com/company/DLinkedList.java
@@ -1,3 +1,12 @@
+/*
+ * Jack Weissenberger February 2018
+ * Double Linked List Project CS 221 Data Structures and Algorithms
+ * This file contains the DLinkedList class which is a doubly linked list which contains the following: Node, isEmpty(),
+ * contains() size(), indexOf(), addFirst(), and toString(). It also contains an Iterator interface the supports
+ * enhanced for loops.
+ */
+
+
 package com.company;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
@@ -5,6 +14,10 @@ import java.util.NoSuchElementException;
 public class DLinkedList implements Iterable<String> {
 
     private static class Node{
+        /*
+         * This node class initializes the data that the node contains, points to the next node and the previous node
+         * and also contains the index of the node
+         */
         private String data;
         private Node next;
         private Node prev;
@@ -18,16 +31,31 @@ public class DLinkedList implements Iterable<String> {
         }
     }
 
+    //here the head and tail nodes are initialized so that they can be used later on as well as initializing the size of
+    //the list to be zero
     Node head;
     Node tail;
     public int size =0;
 
     public DLinkedList(){
+        /*
+         * This initializes the head and the tail to be null which makes an empty list
+         */
         head = null;
         tail = null;
     }
 
     public void addFirst(String data){
+        /*
+         * The addFirst method adds a node at the beginning of the list by making a new head node, having it point to
+         * the previous head node, updating the previous head to point back to it and then making the new node's
+         * previous pointer to be null. This method also increments the overall size of the list by one and increments
+         * the index of all of the following nodes by one (if there are any), so that the indexes are correct.
+         *
+         * Inputs: String data: the string that the new node will be holding
+         * Outputs: N/A, but the list is updated with the new node as the head
+         *
+         */
         head = new Node(data, head, null);
         Node second = head.next;
         if (second != null){ //there is already at least one node
@@ -48,6 +76,16 @@ public class DLinkedList implements Iterable<String> {
     }
 
     public int indexOf(String data){
+        /*
+         * This method returns the index of the node that contains the input data and returns -1 if the data is not
+         * found in the string. It does this by iterating though the list, and checking to see if the string being
+         * searched for is equal to the data stored in each node.
+         *
+         * Inputs: String data: the string that is searched for in the double linked list
+         * Outputs: the integer output of the index of the node that contains the data or -1 if the data is not in the
+         * list
+         *
+         */
         if (head.data.equals(data)){
             return head.indx;
         }
@@ -62,6 +100,14 @@ public class DLinkedList implements Iterable<String> {
     }
 
     public boolean contains(String data){
+        /*
+         * This method returns a boolean True if the input data is in the list and False if it is not. It does this by
+         * checking to see if both the head and tail are defined to be null.
+         *
+         * Inputs: String data: the string that is searched for in the list
+         * Outputs: a boolean that is True if the input data is in the list and False if it is not
+         *
+         */
         if (head.data.equals(data)){
             return true;
         }
@@ -76,6 +122,15 @@ public class DLinkedList implements Iterable<String> {
     }
 
     public String toString(){
+        /*
+         * This method returns all of the data within the list as a single string with no spaces between the words. It
+         * does this by creating a string of the data of the head node and then iterating through the list and concating
+         * all of the following strings
+         *
+         * Inputs: N/A
+         * Outputs: A string with all of the data within the list as a single string with no spaces between the words
+         *
+         */
         String all = head.data;
         Node second = head.next;
         while (second !=null){
@@ -89,6 +144,14 @@ public class DLinkedList implements Iterable<String> {
 
 
     public void printIndx(){
+        /*
+         * This method prints out the all of the indexes of the list, which should be a list from 0 to size-1. It does
+         * this by iterating through the list and printing node.indx of each node.
+         *
+         * Inputs: N/A
+         * Outputs: prints out the all of the indexes of the list, which should be a list from 0 to size-1
+         *
+         */
         System.out.println(head.indx);
         Node second = head.next;
         while (second !=null){
@@ -98,6 +161,13 @@ public class DLinkedList implements Iterable<String> {
     }
 
     public boolean isEmpty(){
+        /*
+         * This method checks to see if the list is empty and returns true if the list is empty and false if it is not
+         *
+         * Inputs: N/A
+         * Outputs: returns true if the list is empty and false if it is not
+         *
+         */
         if (head == null && tail == null){
             return true;
         }
@@ -107,12 +177,24 @@ public class DLinkedList implements Iterable<String> {
     }
 
     public int size(){
+        /*
+         * This method returns the size of the list by printing out the variable size that is incremented by one every
+         * time the addFirst method is called
+         *
+         * Inputs: N/A
+         * Outputs: an integer of the size of the list.
+         */
         return size;
     }
 
 
 
     private class DListIterator implements Iterator<String> {
+        /*
+         * This is the iterator interface that allows for the use of for loops and enhanced for loops to iterate through
+         * the list.
+         *
+         */
         private Node nextNode = head;
 
         @Override
@@ -135,6 +217,9 @@ public class DLinkedList implements Iterable<String> {
 
     @Override
     public Iterator iterator(){
+        /*
+         * This is required for the iterator and returns a new DListIterator
+         */
         return new DListIterator();
     }
 
