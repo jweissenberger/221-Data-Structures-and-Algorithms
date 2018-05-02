a8e0368d66329f62c70a28d95889be5f8c5db81a
John Weissenberger <johnaweissenberger@gmail.com>
2018-05-02 17:11:27 -0400

    commit

--- HashTable/src/com/company/AVLHashSet.java
+++ HashTable/src/com/company/AVLHashSet.java
@@ -2,28 +2,32 @@ package com.company;
 
 public class AVLHashSet<E> {
 
-    private AVLTree<E>[] table = null;
-    //int intArray[];    //declaring array
-    //intArray = new int[20];  // allocating memory to array
+    //initialize the array of AVL trees
+    private AVLTree<E>[] table;
     private int size = 0;
 
+    //Null constructor that initializes the size of the array to 11
     public AVLHashSet(){
         table = new AVLTree[11];
     }
 
+    //constructor that sets the size of the array
     public AVLHashSet(int size){
         table = new AVLTree[size];
     }
 
+    //hashes the data and then stores the information into an AVL tree
     public boolean add(E data){
         //hash the data
-        int hash = data.hashCode();
-        hash = Math.abs(hash);
+        int hash = Math.abs(data.hashCode());
         int index = hash%table.length;
 
+        //creates a new tree if one has not already been created
         if(table[index] == null){
             table[index] = new AVLTree<>();
         }
+
+        //checks the size of the tree before and after we try to add an item so that we can see if it is added
         int s1 = table[index].size;
         table[index].add(data);
         int s2 = table[index].size;
@@ -37,14 +41,14 @@ public class AVLHashSet<E> {
 
     }
 
-
+    //returns the size of the hash table
     public int getSize(){
         return size;
     }
 
+    //checks to see if the inputted data is in the hash table
     public boolean contains(E data){
-        int hash = data.hashCode();
-        hash = Math.abs(hash);
+        int hash = Math.abs(data.hashCode());
         int index = hash%table.length;
 
         if(table[index] == null){
@@ -54,11 +58,10 @@ public class AVLHashSet<E> {
 
     }
 
-
+    //converts the hashtable into an array by traversing each node of the table and then printing out the tree at each
     public E[] toArray(E[] list){
 
         AddToList<E> visitor = new AddToList<E>(list);
-
 
         for (int i = 0; i < table.length; i++){
             if (table[i] != null) {
--- HashTable/src/com/company/AddToList.java
+++ HashTable/src/com/company/AddToList.java
@@ -2,13 +2,16 @@ package com.company;
 
 public class AddToList<E> implements BinaryTreeNode.Visitor<E> {
 
-    public E[] list;
+    //initialize variables
+    private E[] list;
     int i = 0;
 
+    //constructor
     public AddToList(E[] list){
         this.list = list;
     }
 
+    //visit each node in the list
     @Override
     public void visit(BinaryTreeNode<E> node){
         if(list != null && list.length != 0){
